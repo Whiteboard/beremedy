3e738cd9ba613775e59051caea2341f9ba24c4be
Kody Dahl <kody@whiteboard.is>
2013-09-22 16:08:20 -0400

    Splash dev finished

--- js/script.js
+++ js/script.js
@@ -5,3 +5,18 @@
 (function($,w,undefined){
 
 }(jQuery, window));
+
+//clear signup form onClick
+$('input').on('click focusin', function() {
+    this.value = '';
+});
+
+//set window size 
+$(function(){
+    $(window).load(function(){ // On load
+        $('.container, .overlay').css({'height':(($(window).height()))+'px'});
+    });
+    $(window).resize(function(){ // On resize
+        $('.container, .overlay').css({'height':(($(window).height()))+'px'});
+    });
+});
